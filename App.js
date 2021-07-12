85d7dcfd87be4a6ef72319be86211d756bf0a9b5
Milton Palmeira <miltonpalmeira@users.noreply.github.com>
2021-07-12 17:45:53 -0300

    Login Facebook
    Atualização do código de Login com o Facebook com React Native

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Facebook from 'expo-facebook';
 
 export default function App() {
+  const [user, setUser] = useState('');
+
+  const signInFacebook = async () => {
+    try {
+      await Facebook.initializeAsync("499906667782375");
+      const { type, token } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ["public_profile", "email"],
+      });
+      if (type === "success") {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(
+          `https://graph.facebook.com/me?fields=id,name,picture.type(large),email&access_token=${token}`
+        );
+        // console.log((await response.json()).name);
+        const data = await response.json();
+        setUser(data);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {user ? (
+        <View style={styles.fotoContainer}>
+          <Image style={styles.image} source={{uri:user.picture.data.url}} />
+          <Text style={styles.text}>{user.name}</Text>
+          <Text style={styles.text}>{user.email}</Text>
+        </View>
+      ) : (
+        <Button title="Login" onPress={signInFacebook} />
+      )}
     </View>
   );
 }
@@ -18,4 +49,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  fotoContainer: {
+
+  },
+  image: { width: 200, height: 200 },
+  text: { fontSize: 18, textAlign: "center" },
 });
